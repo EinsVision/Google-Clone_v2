40ffab2a502ec117a7f1f35c279f93f80eb71f4a
Changdae <einsvision@gmail.com>
2021-04-13 15:01:29 +0900

    made website structure

--- google-clone-2/pages/index.js
+++ google-clone-2/pages/index.js
@@ -4,11 +4,15 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Google with Next.js and tailwindcss.</h1>  
+      {/* Header */}
+
+      {/* Body */}
+
+      {/* Footer */}
     </div>
   )
 }
