6713985079c1bfc477cfed7a1aa1ac427fee77e3
Changdae <einsvision@gmail.com>
2021-04-15 13:12:04 +0900

    building body component

--- google-clone-2/pages/index.js
+++ google-clone-2/pages/index.js
@@ -35,15 +35,16 @@ export default function Home() {
 
       </header>
       {/* Body */}
-      <form>
+      <form className='flex flex-col items-center mt-40 flex-grow'>
         <Image
           src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
           height={100}
           width={300}
         />
-        <div>
-          <SearchIcon className="h-5 ml-3 text-gray-600" />
-          <input type="text" className="bg-red-500 focus:outline-none"/>
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-600" />
+          <input type="text" className="focus:outline-none flex-grow "/>
+          <MicrophoneIcon className="h-5"/>
         </div>
 
       </form>
