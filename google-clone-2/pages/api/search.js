8c84e2647a46646ac3c9676b0a14ab7d60c2fb13
Changdae <einsvision@gmail.com>
2021-04-16 14:38:38 +0900

    bug report: useRouter in next.js

---
+++ google-clone-2/pages/api/search.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+function Search() {
+  return (
+    <div>
+      <Head>
+        <title>Search Result</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      {/* Header */}
+      <h1>Search Page</h1>
+      {/* Search Results */}
+
+    </div>
+  )
+}
+
+export default Search;
