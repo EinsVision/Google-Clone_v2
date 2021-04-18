13f3ad4dd9f35f2fc8247739e5cce782d52d8350
Changdae <einsvision@gmail.com>
2021-04-18 21:26:58 +0900

    building header component on search page

--- google-clone-2/components/Header.js
+++ google-clone-2/components/Header.js
@@ -16,8 +16,12 @@ function Header() {
         onClick={ () => router.push('/')}
         className='cursor-pointer'
       />
-      <form>
-        <input ref={searchInputRef} type="text"/>
+      <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'>
+        <input 
+          ref={searchInputRef} 
+          type="text"
+          className='flex-grow w-full focus:outline-none'
+        />
       </form>
     </header>
   )
