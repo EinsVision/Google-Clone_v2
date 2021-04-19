6cb14d28ee71ed3fa0e4c74c6bc5a76772e30647
Changdae <einsvision@gmail.com>
2021-04-19 14:25:41 +0900

    building search page results number of results and time

--- google-clone-2/pages/search.js
+++ google-clone-2/pages/search.js
@@ -1,20 +1,24 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import { API_KEY, CONTEXT_KEY } from '../keys';
+import { useRouter } from 'next/router';
+import SearchResults from '../components/SearchResults';
 
 function Search({results}) {
+  const router = useRouter();
   console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Result</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       {/* Header */}
       <Header />
+      
       {/* Search Results */}
-
+      <SearchResults results={results} />
     </div>
   )
 }
