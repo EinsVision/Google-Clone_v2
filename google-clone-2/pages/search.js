fd9eafb81dcea243e656b718707d801f348d1076
Changdae <einsvision@gmail.com>
2021-04-19 14:07:34 +0900

    debugging: fetch url

--- google-clone-2/pages/search.js
+++ google-clone-2/pages/search.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import { API_KEY, CONTEXT_KEY } from '../keys';
 
-function Search() {
+function Search({results}) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -18,3 +20,19 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  
+
+  const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`)
+  .then((response) => response.json())
+  
+
+  //After the SERVER has rendered, pass the results to the client
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
